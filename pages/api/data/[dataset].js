59962747a309c8184157dac585d77fe58eef66cf
Charlie Berens <38572813+charlieberens@users.noreply.github.com>
2022-05-30 21:44:22 -0600

    Updated api filesystem handling

--- pages/api/data/[dataset].js
+++ pages/api/data/[dataset].js
@@ -1,12 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const fs = require("fs").promises;
+const path = require("path");
 
 export default async function handler(req, res) {
     try {
         const { dataset } = req.query;
-        const file_data = await fs.readFile(
+        const dir = path.resolve(
+            "./public",
             `data/${dataset.replace("&", "/")}.json`
         );
+        const file_data = await fs.readFile(dir);
         const json_data = JSON.parse(file_data);
         // Do stuff
         res.status(200).json(json_data);
